1b19ba60474766ffbb6ed972078d57c1871ccaae
a-lvaro <aaflneto@gmail.com>
2022-04-11 21:38:26 -0300

    up

--- cores.py
+++ cores.py
@@ -6,16 +6,15 @@ class Cor:
         while adjacentes:
             pivo = adjacentes.pop(0)
 
-            # print('\n\npivo: ', pivo)
+            # self.__conferirCorAdjacente(pivo)
+
             self.__vertices[pivo - 1].setCor()
-            # print('cor: ', self.__vertices[pivo - 1].getCor())
+
             self.__removeCorAdjacentes(pivo)
 
             for aux in self.__vertices[pivo - 1].getAdjacentes():
                 if type(self.__vertices[aux - 1].getCor()) == list and aux not in adjacentes:
                     adjacentes.append(aux)
-
-            # print('lista percorrer: ', adjacentes)
 
     def __removeCorAdjacentes(self, pivo: int):
         for vertice in self.__vertices[pivo - 1].getAdjacentes():
@@ -23,3 +22,17 @@ class Cor:
             if type(self.__vertices[pivo - 1].getCor()) == list:
                 self.__vertices[vertice -
                                 1].removeCor(self.__vertices[pivo - 1].getCor())
+
+    def __conferirCorAdjacente(self, pivo: int):
+        for vertice in self.__vertices[pivo - 1].getAdjacentes():
+            if type(self.__vertices[vertice - 1].getCor()) != list:
+                if self.__vertices[vertice - 1].getCor() in self.__vertices[pivo - 1].getCor():
+
+                    print(vertice, '   Cor adjacente: ',
+                          self.__vertices[vertice - 1].getCor())
+                    print(pivo, '   Cor pivo:     ',
+                          self.__vertices[pivo - 1].getCor())
+
+                    input()
+                    cor = self.__vertices[vertice - 1].getCor()
+                    self.__vertices[pivo - 1].removeCor(cor)
