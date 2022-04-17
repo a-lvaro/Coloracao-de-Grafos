ede0cca65afdbd9e29eb12bf1892da9fbe2c5033
Rac1805 <romulo.mincache@gmail.com>
2022-04-17 19:03:59 -0300

    up

--- cores.py
+++ cores.py
@@ -1,22 +1,21 @@
 class Cor:
     def __init__(self, vertices: list, listaPrioridade: list) -> None:
-        self.__vertices = vertices
-
         for i in listaPrioridade:
             pivo = i[0]
-            self.__conferirCorAdjacente(pivo)
-            self.__vertices[pivo - 1].setCor()
-            self.__removeCorAdjacentes(pivo)
+            self.__conferirCorAdjacente(vertices, pivo)
+            vertices[pivo - 1].setCor()
+            self.__removeCorAdjacentes(vertices, pivo)
 
-    def __conferirCorAdjacente(self, pivo: int) -> None:
-        for vertice in self.__vertices[pivo - 1].getAdjacente():
-            if type(self.__vertices[vertice - 1].getCor()) != list:
-                if self.__vertices[vertice - 1].getCor() in self.__vertices[pivo - 1].getCor():
-                    self.__vertices[pivo - 1].removeCor(self.__vertices[vertice - 1].getCor())
+    def __conferirCorAdjacente(self, vertices: list, pivo: int) -> None:
+        for vertice in vertices[pivo - 1].getAdjacente():
+            if type(vertices[vertice - 1].getCor()) != list:
+                if vertices[vertice - 1].getCor() in vertices[pivo - 1].getCor():
+                    vertices[pivo -
+                             1].removeCor(vertices[vertice - 1].getCor())
 
-    def __removeCorAdjacentes(self, pivo: int) -> None:
-        for vertice in self.__vertices[pivo - 1].getAdjacente():
+    def __removeCorAdjacentes(self, vertices: list, pivo: int) -> None:
+        for vertice in vertices[pivo - 1].getAdjacente():
             # o adjacente a esse vértice já pode ter uma cor
-            if type(self.__vertices[vertice - 1].getCor()) == list:
-                self.__vertices[vertice -
-                                1].removeCor(self.__vertices[pivo - 1].getCor())
+            if type(vertices[vertice - 1].getCor()) == list:
+                vertices[vertice -
+                         1].removeCor(vertices[pivo - 1].getCor())
